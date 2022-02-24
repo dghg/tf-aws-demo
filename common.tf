aa8467923d92990fcaf4a9cacb92694567271c7a
Donghyeong Kang <dh@uos.ac.kr>
2022-02-24 18:49:21 +0900

    ADD: Subnet

--- common.tf
+++ common.tf
@@ -9,3 +9,13 @@ resource "aws_default_vpc" "default" {
 resource "aws_s3_bucket" "terraform-state-bucket" {
   bucket = "terraform-state-dghg"
 }
+
+resource "aws_subnet" "myprivate" {
+  vpc_id = aws_default_vpc.default.id
+  cidr_block = "172.31.112.0/24"
+}
+
+resource "aws_subnet" "mypublic" {
+  vpc_id = aws_default_vpc.default.id
+  cidr_block = "172.31.212.0/24"
+}
